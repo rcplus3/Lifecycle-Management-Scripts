4d2f762b3c5c6d7b9d5a8a0a4ad65a488b7963d0
Ronald Chao <ronald.chao@plus3it.com>
2025-07-26 16:05:22 -0700

    update

--- Sa.ps1
+++ Sa.ps1
@@ -1,10 +1,12 @@
-# Check the instructions here on how to use it https://github.com/rcplus3/Lifecycle-Management-Script/wiki
+# Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
 
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/rcplus3/Lifecycle-Management-Scripts/main/Sa.cmd'
+#$DownloadURL = 'https://raw.githubusercontent.com/rcplus3/Lifecycle-Management-Scripts/main/Sa.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS.cmd'
+
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
