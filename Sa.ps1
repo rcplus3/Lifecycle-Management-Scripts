ed02f6bc86aaed178d32471fdb29e5953d39566a
Ronald Chao <ronald.chao@plus3it.com>
2025-07-26 16:03:23 -0700

    Update

--- Sa.ps1
+++ Sa.ps1
@@ -1,4 +1,4 @@
-# Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
+# Check the instructions here on how to use it https://github.com/rcplus3/Lifecycle-Management-Script/wiki
 
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
