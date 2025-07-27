513f4dbb2ec494f5f9ed1be8a04b67c5742e585c
Ronald Chao <ronald.chao@plus3it.com>
2025-07-26 21:14:52 -0700

    update

--- IAS.cmd
+++ IAS.cmd
@@ -75,5 +75,17 @@ cls
 
 ::  Check LF line ending
 
+pushd "%~dp0"
+>nul findstr /v "$" "%~nx0" && (
+echo:
+echo Error: Script either has LF line ending issue or an empty line at the end of the script is missing.
+echo:
+ping 127.0.0.1 -n 6 >nul
+popd
+exit /b
+)
+popd
+
+
 ::========================================================================================================================================
 :: Leave empty line below
